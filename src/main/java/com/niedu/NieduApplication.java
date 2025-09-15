7279db021c41264bf213326043b6307a28b9f019
gyesswhat <h2weirdo@icloud.com>
2025-09-15 20:16:37 +0900

    [CHORE] 프로젝트 초기 세팅 (#1)

---
+++ src/main/java/com/niedu/NieduApplication.java
@@ -0,0 +1,13 @@
+package com.niedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NieduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NieduApplication.class, args);
+	}
+
+}
