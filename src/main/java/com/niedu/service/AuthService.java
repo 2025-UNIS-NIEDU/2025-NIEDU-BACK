4b4b699f762d4ce81fea7869f2d8250f00e877b3
gyesswhat <h2weirdo@icloud.com>
2025-10-20 19:57:56 +0900

    [FEAT] 쿠키에서 유저 정보 뽑는 로직 추가 (#28)

---
+++ src/main/java/com/niedu/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.niedu.service;
+
+import com.niedu.entity.user.User;
+import com.niedu.repository.UserRepository;
+import com.niedu.security.jwt.JwtUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    public User getUserFromRequest(HttpServletRequest request) {
+        String token = Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals("accessToken"))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new RuntimeException("Access token not found"));
+
+        Long userId = Long.parseLong(jwtUtil.extractUsername(token));
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+}
