475df8b40d93f51fb81ce13f71ea9be1976c8334
gyesswhat <h2weirdo@icloud.com>
2025-10-23 19:35:59 +0900

    [FEAT] UserAnswerRepository 추가 (#30)

--- src/main/java/com/niedu/repository/learning_record/UserAnswerRepository.java
+++ src/main/java/com/niedu/repository/learning_record/UserAnswerRepository.java
@@ -1,9 +1,13 @@
 package com.niedu.repository.learning_record;
 
+import com.niedu.entity.course.Step;
+import com.niedu.entity.learning_record.StudiedStep;
 import com.niedu.entity.learning_record.user_answer.UserAnswer;
+import com.niedu.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
+    UserAnswer findByStudiedStep(StudiedStep studiedStep);
 }
