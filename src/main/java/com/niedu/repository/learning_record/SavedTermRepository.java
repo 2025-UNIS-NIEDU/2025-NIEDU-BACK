02451b70edfe0c7a7b7e98de51940b8f2eddaf55
gyesswhat <h2weirdo@icloud.com>
2025-10-25 19:07:08 +0900

    [FEAT] SavedCourse, SavedTerm 레포지토리 구현 (#32)

---
+++ src/main/java/com/niedu/repository/learning_record/SavedTermRepository.java
@@ -0,0 +1,18 @@
+package com.niedu.repository.learning_record;
+
+import com.niedu.entity.content.Term;
+import com.niedu.entity.learning_record.SavedTerm;
+import com.niedu.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface SavedTermRepository extends JpaRepository<SavedTerm, Long> {
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    long deleteByUserAndTerm(User user, Term term);
+
+    boolean existsByUserAndTerm(User user, Term term);
+}
