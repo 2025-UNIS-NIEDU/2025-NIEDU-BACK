0a3ff2fef3fe04307a9ef84fe6b707a2f9d7fe04
gyesswhat <h2weirdo@icloud.com>
2025-10-31 20:21:33 +0900

    [REFACT] CourseRepository에서 SavedCourseRepository로 로직 이관 (#43)

--- src/main/java/com/niedu/repository/learning_record/SavedCourseRepository.java
+++ src/main/java/com/niedu/repository/learning_record/SavedCourseRepository.java
@@ -1,10 +1,16 @@
 package com.niedu.repository.learning_record;
 
+import com.niedu.entity.course.Course;
 import com.niedu.entity.learning_record.SavedCourse;
 import com.niedu.entity.learning_record.SavedCourseId;
+import com.niedu.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SavedCourseRepository extends JpaRepository<SavedCourse, SavedCourseId> {
+    Page<Course> findByUserOrderByCourse_CreatedAtDesc(User user, Pageable pageable);
+
 }
