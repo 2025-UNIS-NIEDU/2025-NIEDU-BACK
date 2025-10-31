269f91adfc16da907ea8fd9834283d6ad45a8862
gyesswhat <h2weirdo@icloud.com>
2025-10-31 20:09:32 +0900

    [FIX] HomeCourseRecord 클래스 -> 레코드 리팩토링, List 형태로 감싸는 오버엔지니어링 수정 (#43)

--- src/main/java/com/niedu/dto/home/HomeCourseRecord.java
+++ src/main/java/com/niedu/dto/home/HomeCourseRecord.java
@@ -7,37 +7,23 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class HomeCourseRecord {
-    @Schema(description = "홈 화면 코스 정보 Record")
-    public record CourseInfo(
-            @Schema(description = "썸네일 URL")
-            String thumbnailUrl,
-            @Schema(description = "코스 제목")
-            String title,
-            @Schema(description = "코스 상세 설명")
-            String longDescription,
-            @Schema(description = "토픽명")
-            String topic
-    ) {
-        public static CourseInfo fromEntity(Course course) {
-            return new CourseInfo(
-                    course.getThumbnailUrl(),
-                    course.getTitle(),
-                    course.getDescription(),
-                    course.getTopic() != null ? course.getTopic().getName() : null // Topic 엔티티에서 이름 가져오기
-            );
-        }
-    }
-
-    @Schema(description = "홈 화면 코스 목록 응답 Record")
-    public record CourseListResponse(
-            List<CourseInfo> courses
-    ) {
-        public static CourseListResponse fromEntities(Page<Course> courses) {
-            List<CourseInfo> courseInfos = courses.stream()
-                    .map(CourseInfo::fromEntity)
-                    .collect(Collectors.toList());
-            return new CourseListResponse(courseInfos);
-        }
+@Schema(description = "홈 화면 코스 정보 Record")
+public record HomeCourseRecord (
+        @Schema(description = "썸네일 URL")
+        String thumbnailUrl,
+        @Schema(description = "코스 제목")
+        String title,
+        @Schema(description = "코스 상세 설명")
+        String description,
+        @Schema(description = "토픽명")
+        String topic
+){
+    public static HomeCourseRecord fromEntity(Course course) {
+        return new HomeCourseRecord(
+                course.getThumbnailUrl(),
+                course.getTitle(),
+                course.getDescription(),
+                course.getTopic() != null ? course.getTopic().getName() : null // Topic 엔티티에서 이름 가져오기
+        );
     }
 }
