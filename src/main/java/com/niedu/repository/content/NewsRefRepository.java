22fcd013db741769e9236c9c36046e1411a0af01
gyesswhat <h2weirdo@icloud.com>
2025-10-31 20:21:06 +0900

    [REFACT] 기존 서비스 로직의 불명확한 처리를 개선하기 위해 명확한 메서드 추가 (#43)

--- src/main/java/com/niedu/repository/content/NewsRefRepository.java
+++ src/main/java/com/niedu/repository/content/NewsRefRepository.java
@@ -3,9 +3,21 @@ package com.niedu.repository.content;
 import com.niedu.entity.content.NewsRef;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface NewsRefRepository extends JpaRepository<NewsRef, Long> {
-
-    List<NewsRef> findAllByOrderByIdDesc(Pageable pageable);
+    @Query(
+            value = """
+            SELECT * FROM news_ref
+            WHERE published_at = (
+                SELECT MAX(published_at) FROM news_ref
+            )
+            ORDER BY RANDOM()
+            LIMIT 2
+            """,
+            nativeQuery = true
+    )
+    List<NewsRef> findTwoRandomFromLatestDate();
 }
