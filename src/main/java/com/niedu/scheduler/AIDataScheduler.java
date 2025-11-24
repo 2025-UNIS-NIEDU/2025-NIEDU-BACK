19614ac35294ebaeb26810d31590532fe478f940
seoyeon <2371013@ewhain.net>
2025-11-24 22:05:07 +0900

    [CHORE] data 생성일시 변경

--- src/main/java/com/niedu/scheduler/AIDataScheduler.java
+++ src/main/java/com/niedu/scheduler/AIDataScheduler.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 public class AIDataScheduler {
     private final AIService aiService;
 
-    // 매일 오후 9시 35분 (21:35:00)에 실행되도록 수정
-    @Scheduled(cron = "0 54 21 * * *", zone = "Asia/Seoul")
+    // 매일 오전 7시 (7:00:00)에 실행되도록 수정
+    @Scheduled(cron = "0 7 22 * * *", zone = "Asia/Seoul")
     public void syncAIData() {
         log.info("AI 서버 Data 동기화 시작");
         aiService.syncAIData();
