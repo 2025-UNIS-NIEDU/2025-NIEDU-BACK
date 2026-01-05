f178cc1e0155edf0e3024c9eec73a2bb65e01eb3
Gyeore Kim <h2weirdo@icloud.com>
2026-01-05 22:34:09 +0900

    [HOTFIX] 스케쥴러 시간 오류 수정

--- src/main/java/com/niedu/scheduler/AIDataScheduler.java
+++ src/main/java/com/niedu/scheduler/AIDataScheduler.java
@@ -1,10 +1,12 @@
 package com.niedu.scheduler;
 
-import com.niedu.service.edu.AIService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.niedu.service.edu.AIService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
@@ -13,7 +15,7 @@ public class AIDataScheduler {
     private final AIService aiService;
 
     // 매일 오전 7시 (7:00:00)에 실행되도록 수정
-    @Scheduled(cron = "0 14 19 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 7 * * *", zone = "Asia/Seoul")
     public void syncAIData() {
         log.info("AI 서버 Data 동기화 시작");
         aiService.syncAllAICourses();
